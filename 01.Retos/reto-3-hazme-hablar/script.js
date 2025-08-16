5b603616efb6abb1034e1a64d474ff1984e74a62
Luis Fernando <luisfer.andradesangmail.com>
2025-08-16 13:21:05 -0600

    clase 2

--- 01.Retos/reto-3-hazme-hablar/script.js
+++ 01.Retos/reto-3-hazme-hablar/script.js
@@ -1,17 +1,5 @@
-// Tu misión: hacer que el botón muestre un mensaje cuando se le haga clic
+const boton = document.getElementById("miBoton");
 
-// Paso 1: Selecciona el botón con getElementById
-
-// Paso 2: Agrega un event listener para escuchar el clic
-
-// Paso 3: Cuando se haga clic, muestra un alert que diga: "¡Bien hecho!"
-const { ask } = require('../helpers/input.js');
-
-async function main() {
-    const newButton = document.getElementById('btn-hablar');
-    newButton.addEventListener('click', function() {
-    alert('¡Bien hecho!');
-    });
-}
-
-main();
+boton.addEventListener("click", function() {
+  alert("¡Bien hecho!");
+});
