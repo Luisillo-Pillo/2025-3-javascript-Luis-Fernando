be0e55fd28c6bb3709debd57f82ea7741012f9ed
Luis Fernando <luisfer.andradesangmail.com>
2025-08-18 20:11:02 -0600

    clase 2

--- 02.Clases/Semana2/Ejercicio1.js
+++ 02.Clases/Semana2/Ejercicio1.js
@@ -1,19 +1,18 @@
-const { ask } = requiere('../helpers/input.js');
+const { ask } = require('../helpers/input.js');
 
 async function saludar (nombre) {
-    console.log(`Hola estimad@: ${nombre}, espero que tengas un buen día`)
+    console.log(`Hola estimado (a): ${nombre}, espero que tengas un buen día...`)
 }
 
 async function main () {
-    const personas = [
-        "Andrea",
-        "Dani",
-        "Luis",
-        "Yare",
-    ];
-
-    for(let i = 0; 1 < personas.length;i++){
+    const personas = ["Andrea", "Dani", "Luis", "Yare",];
+    console.log('------------------------Ascendente------------------------');
+    for (let i = 0; 1 < personas.length;i++) {
         saludar(personas[i]);
+    }
+    console.log('------------------------Descendente------------------------');
+    for (let j = personas.length - 1; j >=0; j--) {
+        saludar(personas[j]);
     }
 }
 
