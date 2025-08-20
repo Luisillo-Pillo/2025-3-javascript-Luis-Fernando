de792746c00c16986d7cc6cbdfda5e71abe23883
Luis Fernando <luisfer.andradesangmail.com>
2025-08-20 12:20:34 -0600

    clase 2

--- 02.Clases/Semana2/Ejercicio3.2.js
+++ 02.Clases/Semana2/Ejercicio3.2.js
@@ -8,7 +8,16 @@ async function main() {
         option = await ask(`¿Qué deseas hacer? \n1.- Agregar una fruta. \n2.- Eliminar una fruta. \n3.- Frutas disponibles. \n4.- Salir`)
 
         if(opcion === '1') {
-            let frutaNueva = await ask('¿Que fruta deseas agregar?')
+            let frutaNueva = await ask('¿Que fruta deseas agregar?: ');
+            frutas.push(frutaNueva);
+            console.log('Fruta agregada')
+        } else if (opcion === '2') {
+            if(frutas.length === 0 ) {
+                console.log('No hay frutas disponibles');
+                continue;
+            } else {
+                let frutaEliminar = await ask('')
+            }
         }
     }
 }
