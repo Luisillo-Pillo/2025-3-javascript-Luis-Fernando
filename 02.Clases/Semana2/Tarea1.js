d0733043212f276b724f61b2f2e50ced9423d50b
Luis Fernando <luisfer.andradesangmail.com>
2025-08-21 12:13:41 -0600

    clase 2

--- 02.Clases/Semana2/Tarea1.js
+++ 02.Clases/Semana2/Tarea1.js
@@ -1,12 +1,22 @@
 const { ask } = require('../helpers/input.js');
 
 async function main() {
-    let contador = 1;
-    let limite = 100;
+    //let contador = 1;
+    //let limite = 100;
     
-    for(let i = 2; i <= 100; i+2) {
-        console.log(i)
+    let pares = []
+    
+    for(let i = 0; i <= 100; i = i + 2) {
+        pares.push(i);
     }
+    console.log(`Los números pares del 1 al 100 son: `, pares);
+
+    //let pares = [];
+    //console.log('Ciclo For de 2 en 2');
+    //for (let i = 0; i <= 100; i = i + 2) {
+    //    pares.push(i);
+    //}
+    //console.log('Los numeros pares del 1 al 100 son: ', pares);
 }
 
 main ();
