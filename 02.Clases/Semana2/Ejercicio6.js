a515f293d2b3d308776b250ee14399661da0c0b4
Luis Fernando <luisfer.andradesangmail.com>
2025-08-20 21:35:56 -0600

    clase 2

--- 02.Clases/Semana2/Ejercicio6.js
+++ 02.Clases/Semana2/Ejercicio6.js
@@ -3,12 +3,12 @@ const { ask } = require('../helpers/input.js');
 async function main() {
     const palabra = await ask('Escribe una palabra: ');
     let resultado = '';
-    for(let i = 0; i < palabra.length; i++) {
+    for(let i = palabra.length-1; i >= 0; i--) {
         resultado = resultado + palabra [i]
     }
     
     console.log('Tu palabra al reves es: ', resultado);
-    
+
 }
 
 main ();
