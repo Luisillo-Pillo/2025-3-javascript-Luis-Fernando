97198c39faa4f3301d8cebcb40f5ae527ca3ce09
Luis Fernando <luisfer.andradesangmail.com>
2025-08-25 20:35:12 -0600

    clase 2

---
+++ 02.Clases/Todo-list/main.js
@@ -0,0 +1,8 @@
+"use strict"; //Sirve paraque no se detenga la carga de scripts cuando uno de ellos falla
+
+function addTask() {
+    const input = document.getElementById('taskInput');
+    debugger
+    const taskText = input.ariaValueMax.trim();
+    console.log(taskText);
+}
