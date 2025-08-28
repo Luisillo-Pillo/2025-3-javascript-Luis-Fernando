0802c2d15ded15ac049801e5e6ac69379293b319
Luis Fernando <luisfer.andradesangmail.com>
2025-08-27 19:42:11 -0600

    clase 2

--- 02.Clases/Semana3/StudentManager.js/script.js
+++ 02.Clases/Semana3/StudentManager.js/script.js
@@ -1,3 +1,22 @@
+"use strict";
+
+let students = [];
+
 function addStudent(){
-    
+    const name = document.getElementById('nameInput').ariaValueMax.trim();
+    if(name === ''||name === isNaN) {
+        alert('Debes de capturar un nombre válido');
+        return;
+    }
+    const grade = parseFloat(document.getElementById('gradeInput').value);
+    if (grade > 100 || grade < 0) {
+        alert('Debes capturar una calificación válida')
+        return;
+    }
+
+    const student = {
+        name,
+        grade,
+        status: grade >= 70?'Passed':'Failed',
+    }
 }
